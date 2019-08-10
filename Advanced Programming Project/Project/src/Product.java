889ece39485f91e8b6d586be21f4e73f0f019eb1
arezou <pakzadarezou@gmail.com>
2019-08-10 17:08:33 +0430

    cleaned

--- Advanced Programming Project/Project/src/Product.java
+++
@@ -1,8 +0,0 @@
-public class Product extends Item {
-    public Product(String name, String type, boolean isSpecial) {
-        super(name, type, isSpecial);
-        if(name.equals("Fish")){
-            setPrice(200);
-        }
-    }
-}
